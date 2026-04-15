9d86d43746ed42b8174f7996aad4daec53b4ee50
keiferg37 <200488755@student.georgianc.on.ca>
2026-04-15 15:22:03 -0400

    Remove integrity attribute from Bootstrap JS script in footer.php

--- includes/footer.php
+++ includes/footer.php
@@ -3,7 +3,7 @@
 </footer>
 
 <!-- Bootstrap JS -->
-<script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.8/dist/js/bootstrap.bundle.min.js" integrity="sha384-JpV0cHVPk77lhdqGRG5j6jGFJfYBkVTL7qRvLGx6IkDzYqq6M5y1CSYV" crossorigin="anonymous"></script>
+<script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.8/dist/js/bootstrap.bundle.min.js" crossorigin="anonymous"></script>
 
 <!-- Make sure the cached version of the page isn't shown (in case user is logged out) -->
 <script>
