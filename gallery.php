58587b08053fa0f613bcdb6f2e40e17bded6751c
keiferg37 <200488755@student.georgianc.on.ca>
2026-04-15 15:02:42 -0400

    Add gallery.php to display uploaded images with delete functionality

--- gallery.php
+++ gallery.php
@@ -6,3 +6,19 @@
  * Each image has a Delete button that sends the image ID
  * to delete.php via the URL.
  */
+
+// Make sure the user is logged in before they can access this page
+require "includes/auth.php";
+
+// Connect to the database
+require "includes/connect.php";
+
+// Show the site header
+require "includes/header.php";
+
+// Get all images (newest first)
+$sql = "SELECT * FROM images ORDER BY created_at DESC";
+$stmt = $pdo->prepare($sql);
+$stmt->execute();
+$images = $stmt->fetchAll();
+?>
