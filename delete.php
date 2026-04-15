b9b964ebfd1b6a4a309e6a5bea6059282c8c945c
keiferg37 <200488755@student.georgianc.on.ca>
2026-04-15 15:06:40 -0400

    Add delete.php for image deletion functionality

---
+++ delete.php
@@ -0,0 +1,8 @@
+<?php
+/**
+ * delete.php
+ * ------------------------------------------------------------
+ * Deletes an image record from the database and removes
+ * the image file from the uploads/ folder.
+ * Expects an image ID passed via the URL: delete.php?id=5
+ */
