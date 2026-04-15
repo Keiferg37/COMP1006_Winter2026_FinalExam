4945e83fdcb351c9726670b21131a773be8f3c82
keiferg37 <200488755@student.georgianc.on.ca>
2026-04-15 15:06:50 -0400

    Add initial setup for image deletion in delete.php

--- delete.php
+++ delete.php
@@ -6,3 +6,14 @@
  * the image file from the uploads/ folder.
  * Expects an image ID passed via the URL: delete.php?id=5
  */
+
+// Make sure the user is logged in
+require "includes/auth.php";
+
+// Connect to the database
+require "includes/connect.php";
+
+// Make sure we received an ID in the URL
+if (!isset($_GET['id'])) {
+    die("No image ID provided.");
+}
