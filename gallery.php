d06869a31c4b665972cdd42a212663f26a60ad05
keiferg37 <200488755@student.georgianc.on.ca>
2026-04-15 15:02:32 -0400

    Add gallery.php to display uploaded images with delete functionality

---
+++ gallery.php
@@ -0,0 +1,8 @@
+<?php
+/**
+ * gallery.php
+ * ------------------------------------------------------------
+ * Admin view: displays all uploaded images with their titles.
+ * Each image has a Delete button that sends the image ID
+ * to delete.php via the URL.
+ */
